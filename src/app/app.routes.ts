3fecdc8c1bf4b595e79e457e62fc0bf72a6af086
sreekanthrachamalla9 <sreekanthrachamalla42@gmail.com>
2025-08-21 17:42:30 +0530

    Small changes components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CandidateRegisterComponent } from './pages/register/candidate-register/candidate-register.component';
+import { CandidateLoginComponent } from './pages/login/candidate-login/candidate-login.component';
+import { CandidateComponent } from './pages/dashboard/pages/candidate/candidate.component';
+import { JobsAppliedComponent } from './pages/jobs-applied/jobs-applied.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'register', pathMatch: 'full' },
+    {path: 'register', component: CandidateRegisterComponent},
+    {path:'login', component: CandidateLoginComponent},
+    {path: 'candidate', component: CandidateComponent},
+    {path: 'jobs', component: JobsAppliedComponent}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { HeaderComponent } from './globals/components/header/header.component';
+import { SideNavbarComponent } from './globals/components/side-navbar/side-navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SideNavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('ax-sparkles');
+
+   currentUrl: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl = event.urlAfterRedirects;
+      }
+    });
+  }
+
+  isAuthPage(): boolean {
+    return this.currentUrl === '/login' || this.currentUrl === '/register';
+  }
 }
